49767456e4e26ced80a3fd08b78d5d62e6137037
Samuel Graham <sam.graham@solertium.com>
2022-12-24 13:19:16 -0500

    bcrypt, cors, updates to endpoints

--- server.js
+++ server.js
@@ -1,9 +1,101 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'Sam',
+      email: 'sam@gmail.com',
+      password: 'password',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '456',
+      name: 'Vanessa',
+      email: 'vanessa@gmail.com',
+      password: 'wordpass',
+      entries: 0,
+      joined: new Date(),
+    },
+  ]
+}
 
 app.get('/', (req, res) => {
-  res.send('this is working')
+  res.send(database.users)
 })
+
+app.post('/signin', (req, res) => {
+  const obj = JSON.parse(JSON.stringify(req.body));
+  if (obj.email === database.users[0].email && obj.password === database.users[0].password) {
+    res.json(database.users[0]);
+  } else {
+    res.status(400).json('Error logging in')
+  }
+})
+
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
+  bcrypt.hash(password, null, null, function(err, hash) {
+    console.log(hash);
+  });
+  database.users.push({
+    id: '124',
+    name: name,
+    email: email,
+    entries: 0,
+    joined: new Date(),
+  })
+  res.json(database.users[database.users.length - 1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found) {
+    res.status(400).json('No user found');
+  }
+})
+
+app.put('/image/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++
+      return res.json(user.entries);
+    }
+  })
+  if (!found) {
+    res.status(400).json('No user found');
+  }
+})
+
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//   // Store hash in your password DB.
+// });
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//   // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//   // res = false
+// });
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
