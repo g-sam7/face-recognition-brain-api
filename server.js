c12a8d87c6b4287edca78aeb84449288c301b076
Samuel Graham <sam.graham@solertium.com>
2022-12-30 14:51:23 -0500

    revert, add ssl false

--- server.js
+++ server.js
@@ -18,34 +18,18 @@ const db = knex({
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false
-    }
+    rejectUnauthorized: false
+  }
   }
 });
 
 app.get('/', (req, res) => { res.send("Success!") });
-
-app.get('/db', async (req, res) => {
-  try {
-    const client = await db.connect();
-    const result = await client.query('SELECT * FROM face-recognition-brain-api');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
-
-
-
-// app.post('/signin', signin.handleSignin(db, bcrypt));
-// app.post('/register', register.handleRegister(db, bcrypt));
-// app.get('/profile/:id', profile.handleProfile(db));
-// app.put('/image', image.handleImage(db));
-// app.post('/imageurl', image.handleApiCall());
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt));
+app.get('/profile/:id', profile.handleProfile(db));
+app.put('/image', image.handleImage(db));
+app.post('/imageurl', image.handleApiCall());
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`face recognition brain is running at ${process.env.PORT || 3000}`);
+  console.log(`face recognition brain is running ${process.env.PORT || 3000}`);
 })
