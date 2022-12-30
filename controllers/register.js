1ccfe4db60b582733f70fa30a57a28d40dcde898
Samuel Graham <sam.graham@solertium.com>
2022-12-30 14:34:57 -0500

    remove err

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,7 @@ const handleRegister = (db, bcrypt) => (req, res) => {
       .then(trx.commit)
       .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('Unable to register user', err))
+    .catch(err => res.status(400).json('Unable to register user'))
 };
 
 module.exports = {
